31e1c1e8856e730b9c5fd65a365677aa50b71d36
Ebiladou <oyinebiladouomemu@gmail.com>
2022-08-29 12:23:42 +0100

    changes for desktop view

---
+++ script.js
@@ -0,0 +1,5 @@
+const hamBurgerButton = document.getElementById("hamburger-button")
+const closeButton = document.getElementById("close-button")
+const features = document.getElementsByClassName("features")
+const company = document.getElementsByClassName("company")
+
